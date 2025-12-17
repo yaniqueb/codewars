7195f9253ae8b596f00cbce88110b2ed930aaeb4
yaniqueb <yanique.b@gmail.com>
2025-12-17 10:30:00 -0500

    number to string in python

---
+++ Convert-a-number-to-a-string/number_to_string.py
@@ -0,0 +1,11 @@
+# We need a function that can transform a number (integer) into a string.
+# What ways of achieving this do you know?
+
+# Solution 1: Using Built-in Conversion Function
+def number_to_string(num) :
+    return str(num)
+
+#examples
+print(str(123))   # "123"
+print(str(-456))  # "-456"
+print(str(0))     #"0"
