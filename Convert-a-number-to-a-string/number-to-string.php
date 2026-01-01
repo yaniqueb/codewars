a1d8b98393ed7b5eb54b2a0fb5acfa243adb3784
yaniqueb <yanique.b@gmail.com>
2026-01-01 14:45:22 -0500

    Number to String in php

---
+++ Convert-a-number-to-a-string/number-to-string.php
@@ -0,0 +1,19 @@
+<?php
+// We need a function that can transform a number (integer) into a string.
+// What ways of achieving this do you know? 
+
+//Solution 1: Using the built-in strval() function
+function numberToString($num) {
+  return strval($num);
+}
+
+//examples
+echo strval(100);    // "100"
+echo strval(999);    // "999"
+echo strval(-100);   // "-100"
+
+
+//Solution 2: 
+
+
+?>
