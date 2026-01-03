e2924dae3cefa33c8e30749beec3b7253a8c5fda
yaniqueb <yanique.b@gmail.com>
2026-01-03 10:30:00 -0500

    string to number in js solution 2

--- Convert-a-string-to-a-number/stringToNumber.js
+++ Convert-a-string-to-a-number/stringToNumber.js
@@ -18,3 +18,23 @@ console.log(parseInt("1234"));  // 1234
 console.log(parseInt("605"));   // 605
 console.log(parseInt("1405"));  // 1405
 console.log(parseInt("-7"));    // -7
+
+
+
+// Solution 2: Brute Force
+function stringToNum(num) {
+  let result = 0;
+  let negative = false;
+  let startIndex = 0;
+  // check for negative sign
+  if (str[0]==="-") {
+    isNegative = true;
+    strtIndex = 1;
+  }
+  //process each digit
+  for (let i = startIndex; i < str.lenght; i++ ) {
+    let digit = str[i].charCodeAt (0) - '0'.charCodeAt(0);
+    result = result * 10 + digit;
+  }
+  return isNegative ? -result : result;
+}
