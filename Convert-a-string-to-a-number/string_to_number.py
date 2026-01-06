34c76092a3c8b68e6c4ae93516e2593c7000b5cc
yaniqueb <yanique.b@gmail.com>
2026-01-06 12:57:58 -0500

    string to number in python

---
+++ Convert-a-string-to-a-number/string_to_number.py
@@ -0,0 +1,19 @@
+# We need a function that can transform a string into a number. What ways of achieving this do you know?
+# Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+# Examples
+# "1234"  --> 1234
+# "605"   --> 605
+# "1405"  --> 1405
+# "-7"    --> -7
+
+
+# solution 1: use pythons's built-in parse function
+def string_to_number(s) :
+  return int(s)
+
+#Examples
+print(int("1234"))  # 1234
+print(int("605"))   # 605
+print(int("1405"))  # 1405
+print(int("-7"))    # -7
