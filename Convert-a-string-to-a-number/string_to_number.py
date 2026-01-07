bff5afcea1f7bd708b802ad9209c2f5593fe6cad
yaniqueb <yanique.b@gmail.com>
2026-01-07 17:27:49 -0500

    string to number solution 2 in python

--- Convert-a-string-to-a-number/string_to_number.py
+++ Convert-a-string-to-a-number/string_to_number.py
@@ -17,3 +17,21 @@ print(int("1234"))  # 1234
 print(int("605"))   # 605
 print(int("1405"))  # 1405
 print(int("-7"))    # -7
+
+# Solution 2: Brute force
+def string_to_numbers(s) :
+  result = 0
+  is_negative = False
+  start_index = 0
+
+  # check for negative sign
+  if s[0] == '-':
+    is_negative = True
+    start_index = 1
+  
+  # Process each digit
+  for i in range(start_index, len(s)):
+    digit = ord(s[i]) - ord('0') # convert char to digit
+    result = result * 10 + digit
+
+  return -result if is_negative else result
