17607addabcc3f855818f45d68c45c561c674c90
yaniqueb <yanique.b@gmail.com>
2026-01-13 13:24:25 -0500

    brute force solution for split strings in python

--- Duplicate-Encoder/duplicate_encoder.py
+++ Duplicate-Encoder/duplicate_encoder.py
@@ -1,15 +1,15 @@
 """
-#The goal of this exercise is to convert a string to a new string
-#where each character in the new string is "(" 
-#if that character appears only once in the original string, 
-#or ")" if that character appears more than once in the original string. 
-#Ignore capitalization when determining if a character is a duplicate.
+The goal of this exercise is to convert a string to a new string
+where each character in the new string is "(" 
+if that character appears only once in the original string, 
+or ")" if that character appears more than once in the original string. 
+Ignore capitalization when determining if a character is a duplicate.
 
-#Examples
-# "din"      =>  "(((" (all unique)
-#"recede"   =>  "()()()" (r=once,e=3times,c=once,e=3times, )
-#"Success"  =>  ")())())"
-#"(( @"     =>  "))(("
+Examples
+"din"      =>  "(((" (all unique)
+"recede"   =>  "()()()" (r=once,e=3times,c=once,e=3times, )
+"Success"  =>  ")())())"
+"(( @"     =>  "))(("
 """
 def duplicate_encode(word):
     # Convert to lowercase so 'A' and 'a' are treated the same
---
+++ Split Strings/split_string.py
@@ -0,0 +1,24 @@
+"""
+Complete the solution so that it splits the string into pairs of two characters. If the string contains 
+an odd number of characters then it should replace the missing second character of the final pair with an 
+underscore ('_'). 
+
+Examples
+"abc"     => ['ab', 'c_']
+"abcedf"  => ['ab', 'cd', 'ef']
+"a"       => ['a_']
+""        => []
+"""
+
+# solution 1: Loop through the string manually, grabbing 2 characters at a time, and handle the odd case at the end.
+def split_strings(s):
+  result = []
+
+  for i in range(0, len(s), 2):
+    pair = s[i + 1] # Add second character if it exists
+  else:
+    pair += '_' #Add underscore if second character dosen't exist
+    
+    result.append(pair)
+
+  return result
