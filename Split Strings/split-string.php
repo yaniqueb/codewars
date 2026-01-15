4bd402c27e1985d33fc1f33ff103fa887d2c8593
yaniqueb <yanique.b@gmail.com>
2026-01-14 19:25:15 -0500

    manual loop of split strings in php

---
+++ Split Strings/split-string.php
@@ -0,0 +1,38 @@
+<?php
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains 
+// an odd number of characters then it should replace the missing second character of the final pair with an 
+// underscore ('_'). 
+
+// Examples
+// "abc"     => ['ab', 'c_']
+// "abcedf"  => ['ab', 'cd', 'ef']
+// "a"       => ['a_']
+// ""        => []
+
+// Solution 1: Loop through the string manually
+function splitString($str) {
+  $result = [];
+
+  for ($i = 0; $i < strlen($str); $i += 2) {
+    $pair = $str[$i]; // Get first character
+
+    if ($i + 1 < strlen($str)) {
+      $pair . $str[$i + 1]; // Add second character if it exists
+    } else {
+      $pair . '_'; // Add underscore if second character dosen't exist
+    }
+    $result[] = $pair;
+  }
+  return result;
+}
+
+
+
+
+
+
+
+
+
+
+?>
