f3441392e78597f755e1067543c9daf652bc578d
yaniqueb <yanique.b@gmail.com>
2026-01-26 22:08:04 -0500

    added simplified solution for reverse words in python

--- Reverse-words/reverse_words.py
+++ Reverse-words/reverse_words.py
@@ -1,3 +1,12 @@
+"""
+Docstring for Reverse-words.reverse_words
+Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+"""
+
 # solution 1: Brute force
 def reverse_words(text):
   result = ''
@@ -20,3 +29,8 @@ def reverse_words(text):
 print(reverse_words('The quick brown fox jumps over the lazy dog.'))
 print(reverse_words('apple'))
 print(reverse_words('  double  spaced  words  '))
+
+# solution 2: clean/readable
+def reverse_word(text):
+  # split by spaces, reverse each word, join back
+  return ' '.join(word[::-1]for word in text.split(' '))
