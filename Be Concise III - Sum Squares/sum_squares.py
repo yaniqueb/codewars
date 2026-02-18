b9cc7d33f2141a28137b39edefbc37adaafa49c7
yaniqueb <yanique.b@gmail.com>
2026-02-18 17:26:49 -0500

    solution 2 for sum squares in python

--- Be Concise III - Sum Squares/sum_squares.py
+++ Be Concise III - Sum Squares/sum_squares.py
@@ -4,3 +4,13 @@ You are given a program sumSquares that takes an array as input and returns the 
 # solution 01: Use array methods to map(square) then reduce(sum).
 def sum_squares(array):
   return sum(map(lambda x: x ** 2, array))
+
+# solution 02: combine squaring and summing in a single reduce operation
+from functools import reduce
+
+def sum_square(array):
+  return reduce(lambda sum, num: sum + num ** 2, array, 0)
+
+# but python's sum() with generator is more idiomatic
+def sum_squares(array):
+  return sum(x ** 2 for x in array)
